f3292d6d6b3cd37ddeb1039fffe69b79b7b00ba3
Alex <play.io@protonmail.ch>
2024-12-02 14:50:05 +0900

    Add azure network helper & certs

---
+++ main/azure/network_helper.cpp
@@ -0,0 +1,101 @@
+#include <azure_iot_hub_client.h>
+#include <azure_iot_provisioning_client.h>
+#include <backoff_algorithm.h>
+#include <transport_tls_socket.h>
+#include <transport_abstraction.h>
+
+#include "certs.h"
+#include "config.h"
+#include "network_helper.h"
+
+#define RAND32()    ( rand() / RAND_MAX )
+
+/* Each compilation unit must define the NetworkContext struct. */
+struct NetworkContext
+{
+    void * pParams;
+};
+
+static const char* TAG = "AzureNetworkHelper";
+static uint8_t shared_mqtt_message_buffer[MQTT_MESSAGE_BUFFER_SIZE] = { 0 };
+
+uint8_t* get_shared_mqtt_message_buffer(uint32_t* buffer_size)
+{
+    *buffer_size = MQTT_MESSAGE_BUFFER_SIZE;
+    return shared_mqtt_message_buffer;
+}
+
+/**
+* @brief Connect to server with backoff retries.
+*/
+uint32_t ConnectToServerWithBackoffRetries( const char * pcHostName,
+                                                      uint32_t port,
+                                                      NetworkCredentials_t * pxNetworkCredentials,
+                                                      NetworkContext_t * pxNetworkContext )
+{
+    TlsTransportStatus_t xNetworkStatus;
+    BackoffAlgorithmStatus_t xBackoffAlgStatus = BackoffAlgorithmSuccess;
+    BackoffAlgorithmContext_t xReconnectParams;
+    uint16_t usNextRetryBackOff = 0U;
+
+    /* Initialize reconnect attempts and interval. */
+    BackoffAlgorithm_InitializeParams( &xReconnectParams,
+                                       AZURE_IOT_RETRY_BACKOFF_BASE_MS,
+                                       AZURE_IOT_RETRY_MAX_BACKOFF_DELAY_MS,
+                                       AZURE_IOT_RETRY_MAX_ATTEMPTS );
+
+    /* Attempt to connect to IoT Hub. If connection fails, retry after
+     * a timeout. Timeout value will exponentially increase till maximum
+     * attempts are reached.
+     */
+    do
+    {
+        LogInfo( ( "Creating a TLS connection to %s:%lu.\r\n", pcHostName, port ) );
+        /* Attempt to create a mutually authenticated TLS connection. */
+        xNetworkStatus = TLS_Socket_Connect( pxNetworkContext,
+                                             pcHostName, port,
+                                             pxNetworkCredentials,
+                                             AZURE_IOT_TRANSPORT_SEND_RECV_TIMEOUT_MS,
+                                             AZURE_IOT_TRANSPORT_SEND_RECV_TIMEOUT_MS );
+
+        if( xNetworkStatus != eTLSTransportSuccess )
+        {
+            /* Generate a random number and calculate backoff value (in milliseconds) for
+             * the next connection retry.
+             * Note: It is recommended to seed the random number generator with a device-specific
+             * entropy source so that possibility of multiple devices retrying failed network operations
+             * at similar intervals can be avoided. */
+            xBackoffAlgStatus = BackoffAlgorithm_GetNextBackoff( &xReconnectParams, RAND32(), &usNextRetryBackOff );
+
+            if( xBackoffAlgStatus == BackoffAlgorithmRetriesExhausted )
+            {
+                LogError( ( "Connection to the IoT Hub failed, all attempts exhausted." ) );
+            }
+            else if( xBackoffAlgStatus == BackoffAlgorithmSuccess )
+            {
+                LogWarn( ( "Connection to the IoT Hub failed [%d]. "
+                           "Retrying connection with backoff and jitter [%d]ms.",
+                           xNetworkStatus, usNextRetryBackOff ) );
+                vTaskDelay( pdMS_TO_TICKS( usNextRetryBackOff ) );
+            }
+        }
+    } while( ( xNetworkStatus != eTLSTransportSuccess ) && ( xBackoffAlgStatus == BackoffAlgorithmSuccess ) );
+
+    return xNetworkStatus == eTLSTransportSuccess ? 0 : 1;
+}
+/*-----------------------------------------------------------*/
+
+/**
+* @brief Setup transport credentials.
+*/
+uint8_t SetupNetworkCredentials( NetworkCredentials_t* pxNetworkCredentials )
+{
+    pxNetworkCredentials->xDisableSni = pdFALSE;
+
+    /* Set the credentials for establishing a TLS connection. */
+    pxNetworkCredentials->pucRootCa = ( const unsigned char * ) AZURE_IOT_ROOT_CA_PEM;
+    pxNetworkCredentials->xRootCaSize = sizeof( AZURE_IOT_ROOT_CA_PEM );
+
+    return 0;
+}
+/*-----------------------------------------------------------*/
---
+++ main/azure/network_helper.h
@@ -0,0 +1,20 @@
+#ifndef _H_AZURE_NETWORK_HELPER_H_
+#define _H_AZURE_NETWORK_HELPER_H_
+
+#define AZURE_IOT_RETRY_MAX_ATTEMPTS                    ( 5U )
+#define AZURE_IOT_RETRY_MAX_BACKOFF_DELAY_MS            ( 5000U )
+#define AZURE_IOT_RETRY_BACKOFF_BASE_MS                 ( 500U )
+
+#define AZURE_IOT_TRANSPORT_SEND_RECV_TIMEOUT_MS        ( 2000U )
+#define MQTT_MESSAGE_BUFFER_SIZE                        ( 5 * 1024U )
+
+uint8_t* get_shared_mqtt_message_buffer();
+
+uint32_t ConnectToServerWithBackoffRetries( const char * pcHostName,
+                                                      uint32_t port,
+                                                      NetworkCredentials_t * pxNetworkCredentials,
+                                                      NetworkContext_t * pxNetworkContext );
+
+uint8_t SetupNetworkCredentials( NetworkCredentials_t* pxNetworkCredentials );
+
+#endif
